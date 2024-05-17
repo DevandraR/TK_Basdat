0a0bcf6f9911426a78853dc31a4fd1ee9a430a58
luhurb <168656675+luhurb@users.noreply.github.com>
2024-05-17 18:37:57 +0700

    Update urls.py

--- marmut/urls.py
+++ marmut/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = 'marmut'
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('subscribe/', views.subscribe, name='subscribe'),
+    path('pembayaran/', views.pembayaran_view, name='pembayaran'),
+    path('downloadedsongs/', views.downloaded_songs, name='downloadedsongs'),
+    path('downloadedsong/', views.downloaded_songs, name='downloadedsongs'),
+    path('searchfind/', views.search_find, name='searchfind'),
+
 ]
