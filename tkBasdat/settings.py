1a22365fa1c5d98deedbe734fe5afab5344e7386
DevandraR <devandra271203@gmail.com>
2024-05-17 21:33:42 +0700

    added vercel config

--- tkBasdat/settings.py
+++ tkBasdat/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-c^(qsr6gvzk_5%-08ez=84!+k0o2iy5q2otzro8r@==3)x_7c%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '.vercel.app']
 
 # Application definition
 
